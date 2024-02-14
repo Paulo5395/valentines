25c059cf962d5b8ac50ffaaed53668f5607f8dc6
Paulo5395 <paulolange805@gmail.com>
2024-02-13 22:22:03 -0800

    commit

--- myscript.js
+++ myscript.js
@@ -1,1 +1,17 @@
+$(document).ready(function() {
+	$("#question").click(function(){
+		$("#reveal").slideDown("slow");
+            $("#responses").slideDown("slow");
+                $("#yes").delay(800).slideDown();
+                $("#yes2").delay(1000).slideDown();
+	});
 
+    $("#yes").click(function(){
+        $("#end").slideDown("slow");
+        $("img").fadeIn();
+    });
+    $("#yes2").click(function(){
+        $("#end").slideDown("slow");
+        $("img").fadeIn();
+    });
+});
